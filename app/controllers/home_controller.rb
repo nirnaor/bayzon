9638496b334ff85351e014cf1a357c560d0d20f3
nirnaor <nirnaori@gmail.com>
2016-07-23 13:39:11 +0300

    Handling exceptions from eBay and Amazon.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,23 @@ class HomeController < ApplicationController
 
   def fetch_sellers
     query = params[:query]
-    product_name = product_name(query)
-    @products = find_sellers(product_name)
-    render :index
+    begin
+      @product_name = product_name(query)
+    rescue Exception => ex
+      logger.error ex.backtrace
+      flash[:warning] = "There was a problem fetching the product name from ebay"
+      render :search and return
+    end
+
+    begin
+      @products = find_sellers(@product_name)
+      flash[:success] = "Displaying sellers in Amazon for product #{@product_name}"
+      render :index
+    rescue Exception => ex
+      logger.error ex.backtrace
+      flash[:warning] = "There was a problem fetching sellers from Amazon for product named '#{@product_name}'"
+      render :search and return
+    end
   end
 
   def index
