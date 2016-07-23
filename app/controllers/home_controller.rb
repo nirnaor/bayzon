d14a694ad504c6d4e5ab76a392350b39ae4e6ea5
nirnaor <nirnaori@gmail.com>
2016-07-23 12:38:50 +0300

    Displaying Amazon's results based on ebay's
    product name.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
+require 'amazon'
+require 'ebay'
+
 class HomeController < ApplicationController
   def search
+  end
+
+  def fetch_sellers
+    query = params[:query]
+    product_name = product_name(query)
+    @products = find_sellers(product_name)
+    render :index
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'home#search'
 
   get 'home/index'
+  get 'fetch_sellers', to: "home#fetch_sellers"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
