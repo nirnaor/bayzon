7fde9397c7b8d0c18c56e5bc1f801b66dd50f956
nirnaor <nirnaori@gmail.com>
2016-07-23 22:45:00 +0300

    Configured Heroku to use ActionCable.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # https://mysterious-mountain-23299.herokuapp.com
+  config.web_socket_server_url = "wss://mysterious-mountain-23299.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://mysterious-mountain-23299.herokuapp.com', 'http://mysterious-mountain-23299.herokuapp.com' ]
+
+
 end
